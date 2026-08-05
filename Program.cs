f318f0684d71b7ca2a1afec227589e411c722b7e
emanosalvas-remar <68337935+emanosalvas-remar@users.noreply.github.com>
2026-08-04 19:54:25 -0500

    OK

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Herencia.Clases;
+
+Persona objPersona1 = new Persona("0926826505", "Eduardo", "Manosalvas", new DateOnly(1993, 9, 6));
+objPersona1.saludar();
+
+Console.ReadLine();
+
+
