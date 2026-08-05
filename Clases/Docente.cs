aeba4930325284b8f5e40bb5cf7f381272629a01
emanosalvas-remar <68337935+emanosalvas-remar@users.noreply.github.com>
2026-08-04 20:13:50 -0500

    Añadir clases Estudiante, Docente y DirectorCarrera
    Se han implementado las clases `Estudiante`, `Docente` y `DirectorCarrera`, cada una con métodos específicos para saludar y realizar acciones. Se han actualizado los mensajes de consola en `Program.cs` para mostrar información sobre los objetos creados. Además, se han definido propiedades relevantes en cada clase, como `Facultad` en `DirectorCarrera`, `Asignatura` y `AñosExperiencia` en `Docente`, y `Carrera` y `Semestre` en `Estudiante`.

---
+++ Clases/Docente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia.Clases
+{
+    public class Docente:Persona
+    {
+        public string Asignatura { get; set; }
+        public int AñosExperiencia { get; set; }
+        public Docente(string cedula, string nombre, string apellido, DateOnly fechaNacimiento, string asignatura, int añosExperiencia)
+            : base(cedula, nombre, apellido, fechaNacimiento)
+        {
+            Asignatura = asignatura;
+            AñosExperiencia = añosExperiencia;
+        }
+        public void enseñar()
+        {
+            Console.WriteLine($"Soy {Nombre} {Apellido}, enseño la asignatura de {Asignatura} y tengo {AñosExperiencia} años de experiencia.");
+        }
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,8 +2,30 @@
 using Herencia.Clases;
 
 Persona objPersona1 = new Persona("0926826505", "Eduardo", "Manosalvas", new DateOnly(1993, 9, 6));
+
+Console.WriteLine("Persona 1");
 objPersona1.saludar();
 
-Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine("Estudiante 1");
+Estudiante objEstudiante1 = new Estudiante("0926826505", "Eduardo", "Manosalvas", new DateOnly(1993, 9, 6), "Desarrollo de Software", 2);
+objEstudiante1.saludar();
+objEstudiante1.estudiar();
 
+Console.WriteLine();
+Console.WriteLine("Docente 1");
+Docente objDocente1 = new Docente("0926826505", "Eduardo", "Manosalvas", new DateOnly(1993, 9, 6), "Programación", 5);
+objDocente1.saludar();
+objDocente1.enseñar();
+
+
+Console.WriteLine();
+Console.WriteLine("Director 1");
+DirectorCarrera objDirectorCarrera = new DirectorCarrera("0926826505", "Eduardo", "Manosalvas", new DateOnly(1993, 9, 6), "Programación", 5, "Ingeniería de Software");
+objDirectorCarrera.saludar();
+objDirectorCarrera.enseñar();
+objDirectorCarrera.dirigir();
+
+
+Console.ReadLine();
 
